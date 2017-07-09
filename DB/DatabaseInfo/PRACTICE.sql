aefc3543b60f19cb9d9367c3d1c9c00fce931aa1
muktadir <muktadir.rhmn@gmail.com>
2017-07-09 12:21:45 +0600

    last

---
+++ DB/DatabaseInfo/PRACTICE.sql
@@ -0,0 +1,15 @@
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (41, 0, NULL, 'Desert Fathers ', 'eats locusts ', NULL);
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (42, 0, NULL, 'I am ', 'a boy ', NULL);
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (43, 0, NULL, 'I am ', 'a man ', NULL);
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (61, 0, NULL, 'I ', 'am tired ', NULL);
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (62, 2, 'Are you  ____  christian', 'a', 'an', 'the');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (63, 2, 'You are  ____  idiot', 'an', 'the', 'a');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (64, 2, 'I am  ____  idiot', 'an', 'the', 'a');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (65, 2, 'She is  ____  idiot', 'an', 'the', 'a');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (3, 1, 'I went  ____  a doctor', 'past', 'to', 'onto');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (5, 1, 'I will borrow the book  ____  your house', 'for', 'from', 'unlike');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (6, 1, 'I will borrow the book  ____  your house', 'from', 'upon', 'as');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (7, 1, 'Your are going  ____  a book store', 'upon', 'to', 'but');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (8, 1, 'I was going  ____  your home', 'upon', 'to', 'under');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (21, 1, 'I am  ____  Bangladesh', 'from', 'for', 'above');
+INSERT INTO "PRACTICE" ("PRACTICE_ID", "ENTITY_ID", "PROBLEM", "OPTION1", "OPTION2", "OPTION3") VALUES (22, 1, 'You are  ____  Uganda', 'from', 'against', 'per');
